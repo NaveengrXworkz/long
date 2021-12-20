25a53c00443c7814ab89cd62b6845b024969ba28
NaveengrXworkz <naveengr.xworkz@gmail.com>
2021-12-20 21:42:41 +0530

    long programme

---
+++ Long.java
@@ -0,0 +1,15 @@
+class Long{
+     static long []contactno = {12345678,987654318,987634,987654,456777,9865443,3542576,465255,6233486,1264723};
+     public static void main(String[] args){
+     System.out.println(contactno[0]);
+     System.out.println(contactno[1]);
+     System.out.println(contactno[2]); 
+     System.out.println(contactno[3]);
+     System.out.println(contactno[4]);
+     System.out.println(contactno[5]);
+     System.out.println(contactno[6]);
+     System.out.println(contactno[7]);
+     System.out.println(contactno[8]);
+     System.out.println(contactno[9]);
+     }
+}
